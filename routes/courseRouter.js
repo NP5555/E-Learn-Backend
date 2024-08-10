51d6227afb53d73d4f038e5da651977864f2b5e9
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-08-10 07:03:53 -0700

    small updates inside vs

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -3,21 +3,12 @@ const courseControl = require("../controller/courseControl");
 const { verifyUserToken } = require('../middleware/authUser');
 
 
-<<<<<<< HEAD
-router.get("/courses", courseControl.searchApi),
-router.get("/short-details", courseControl.shortDetails),
-router.get("/serachById/:id", courseControl.searchByID),
-router.get("/catagories", courseControl.catagories);
-router.get("/search", courseControl.searchApi),
-router.get("/serach/categories", courseControl.searchCategory);
-=======
 router.get("/courses",verifyUserToken, courseControl.searchApi),
 router.get("/featured",verifyUserToken, courseControl.featured),
 router.get("/searchById/:id",verifyUserToken, courseControl.searchByID),
 router.get("/catagories",verifyUserToken, courseControl.catagories);
 router.get("/search",verifyUserToken, courseControl.search);
 router.get("/serach/categories",verifyUserToken, courseControl.searchCategory);
->>>>>>> upstream/main
 router.get("/saved", verifyUserToken, courseControl.getSavedCourse) 
 router.post("/saved", verifyUserToken, courseControl.addSaved) 
 router.delete("/saved", verifyUserToken, courseControl.deleteSaved) 
