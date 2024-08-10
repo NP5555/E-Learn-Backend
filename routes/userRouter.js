55082053a0679f99cc8d5e72549770abbaa01236
Hamid <hamidjaved615@gmail.com>
2024-08-10 12:55:38 +0500

    Added change user details API

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,7 @@ const { verifyUserToken } = require("../middleware/authUser");
 router.post("/register", authControll.register);
 router.post("/signin-google", authControll.googleAuth);
 router.post("/login", authControll.login);
+router.post("/change-details", verifyUserToken , authControll.changeDetails)
 router.post("/signout", authControll.SignOut);
 router.post("/request-otp", authControll.requestOtp);
 router.post("/reset-password", authControll.resetPassword);
